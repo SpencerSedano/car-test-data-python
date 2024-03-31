7687bb85543e0246b749b291af2730614425ec0d
SpencerSedano <spencer.sv20@gmail.com>
2024-03-31 18:21:07 +0800

    adding to files

---
+++ main_python.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+
+df = pd.read_csv(
+    "C:/Users/spenc/OneDrive/Desktop/python-car-test-data/true-false-questions/output/output3_all_pages.csv",
+    on_bad_lines="warn",
+)
---
+++ main_tabula.py
@@ -0,0 +1,32 @@
+""" from PyPDF2 import PdfReader
+
+reader = PdfReader(
+    "C:/Users/spenc/OneDrive/Desktop/python-car-test-data/true-false-questions/file.pdf"
+)
+count = 0
+number_of_pages = len(reader.pages)
+print(f"This is the number of PAGES: {number_of_pages}")
+for i in range(number_of_pages):
+    page = reader.pages[i]
+    text = page.extract_text()
+    print(text)
+    count += 1
+
+print(f"The loop went: {count} times")
+
+ """
+
+import tabula
+
+""" df = tabula.read_pdf(
+    "C:/Users/spenc/OneDrive/Desktop/python-car-test-data/true-false-questions/file.pdf",
+    pages="all",
+) """
+
+tabula.convert_into(
+    "C:/Users/spenc/OneDrive/Desktop/python-car-test-data/true-false-questions/file.pdf",
+    "output3_all_pages.csv",
+    output_format="csv",
+    pages="all",
+    lattice=True,
+)
